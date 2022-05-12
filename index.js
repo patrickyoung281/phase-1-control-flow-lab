0ced521a753510f4af694cc00e07be89f4e99412
Patrick Young <patrickyoung281@gmail.com>
2022-05-12 10:37:26 -0400

    commit tree 2a68eede27e039caa01e11afdbd88d692e307e9d

--- index.js
+++ index.js
@@ -1,11 +1,89 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet (distance) {
+  if (distance <= 400) {
+    return "This one is on me!"
+  }
+  else if (distance >400 && distance <= 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (distance > 2000 && distance <2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (distance >=2500) {
+    return "No can do."
+  }
+};
+
+
+function ternaryCheckCity (city) {
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }
+  else return "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+function switchOnCharmFromTip (tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much."
+      break;
+    case "not as generous":
+      return "Thank you."
+    default: 
+      return "Bye."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+function scuberGreetingForFeet(rideLength) {
+  if (rideLength<400) {
+    return "This one is on me!"}
+  if (rideLength > 400 && rideLength < 2000) {
+    return "That will be twenty bucks."}
+  if (rideLength > 2000 && rideLength < 2500) {
+    return "I will gladly take your thirty bucks."}
+  if (rideLength > 2500) {
+    return "No can do."} 
+};
+
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."}
+  else {return "No go."}
+};
+
+function switchOnCharmFromTip(tip){
+  if (tip === "generous") {
+    return "Thank you so much."}
+  if (tip ==="not as generous") {
+    return "Thank you."}
+  else {return "Bye."}
+};
+
+*/
